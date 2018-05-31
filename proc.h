3314c32ac5e6a1c40ae0eec3f0565dd1e8e87c70
kazaller <35352091+kazaller@users.noreply.github.com>
2018-05-31 11:29:12 -0700

    added stackSize

--- proc.h
+++ proc.h
@@ -38,6 +38,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 struct proc {
   uint sz;                     // Size of process memory (bytes)
   uint stackSpot; // CS 153
+  uint stackSize; // CS 153
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
