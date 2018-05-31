32d585dde1f2fce90b0537b2752afd74a4ed1f9d
kazaller <35352091+kazaller@users.noreply.github.com>
2018-05-31 12:16:03 -0700

    Update vm.c

--- vm.c
+++ vm.c
@@ -336,7 +336,7 @@ copyuvm(pde_t *pgdir, uint sz)
       goto bad;
   }
   
-  for(i = stackSize; i < stackSpot; i += PGSIZE){ // CS 153
+  for(i = (stackSpot - stackSize*PGSIZE); i < stackSpot; i += PGSIZE){ // CS 153
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
     if(!(*pte & PTE_P))
