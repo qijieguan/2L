52086e58f4a6486191ffc1032066ccc22ed96069
kazaller <35352091+kazaller@users.noreply.github.com>
2018-05-31 10:38:08 -0700

    added StackSpot

--- proc.h
+++ proc.h
@@ -37,6 +37,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   uint sz;                     // Size of process memory (bytes)
+  uint stackSpot; // CS 153
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
