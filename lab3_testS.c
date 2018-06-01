27d0a8a7fc13dad83574fff6f84ef077683dbdf3
kazaller <35352091+kazaller@users.noreply.github.com>
2018-06-01 15:19:23 -0700

    Create lab3_testS.c

---
+++ lab3_testS.c
@@ -0,0 +1,25 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int fib(int n)
+{
+  if (n == 0)
+    return 0;
+  if (n == 1)
+    return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char *argv[])
+{
+   int pid=0;
+   pid=fork();
+   if(pid==0){
+       fib(1);
+       exit();
+   }
+   wait();
+   exit();
+}
