5caad0b62a825c726c7f3ce35b405238fdaab7f5
kazaller <35352091+kazaller@users.noreply.github.com>
2018-06-01 16:24:47 -0700

    Update lab3_testS.c

--- lab3_testS.c
+++ lab3_testS.c
@@ -1,9 +1,12 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include <iostream>
+using namespace std;
 
 int recursive(int n)
 {
+  cout << "In recursive function" << endl;
   if (n == 0)
     return 0;
   return recursive(n);
@@ -14,7 +17,7 @@ int main(int argc, char *argv[])
    int pid=0;
    pid=fork();
    if(pid==0){
-       recursive(2147483647);
+       recursive(1);
        exit();
    }
    wait();
