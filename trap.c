b29014b40b6281d714a8df024787f8f39422d8d9
kazaller <35352091+kazaller@users.noreply.github.com>
2018-06-01 16:41:24 -0700

    Update trap.c

--- trap.c
+++ trap.c
@@ -115,6 +115,7 @@ trap(struct trapframe *tf)
     }
     myproc()->stackSize++;
     cprintf("A page was added to the stack.\n");
+    cprintf("The stackSize is now %d\n", myproc()->stackSize);
   
     break;
   }
