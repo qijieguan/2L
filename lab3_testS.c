abf77d59708dbbe0118d60e1c6fdda01945eb626
kazaller <35352091+kazaller@users.noreply.github.com>
2018-06-01 16:27:05 -0700

    Update lab3_testS.c

--- lab3_testS.c
+++ lab3_testS.c
@@ -1,12 +1,9 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-#include <iostream>
-using namespace std;
 
 int recursive(int n)
 {
-  cout << "In recursive function" << endl;
   if (n == 0)
     return 0;
   return recursive(n);
